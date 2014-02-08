2c2b088e2a8a16763a69e08b3db79ddfd9059185
Brian Zhang <tangerinewhite32@gmail.com>
2014-02-08 05:58:53 +0000

    return the scraped response on localhost:8888

--- zhihu-user.js
+++ zhihu-user.js
@@ -1,28 +1,34 @@
 var request = require("request");
 var cheerio = require("cheerio");
+var http = require("http");
  
-var username = process.argv[0];
+var username = process.argv[2];
 var user = {};
 request({ uri: "http://www.zhihu.com/people/" + username, }, parseZhihuUser);
 
 function parseZhihuUser(error, response, body) {
-  $ = cheerio.load(body);
-  user["name"] = $("span.name").text();
-  user["location"] = $("span.location").attr('title');
-  var followDiv = $("div.zm-profile-side-following > a > strong");
-  var followList = serialize($, followDiv);
-  user["following"] = followList[0];
-  user["followers"] = followList[1];
-  var userStatsDiv = $("div.profile-navbar > a.item > span.num");
-  var userStatsList = serialize($, userStatsDiv);
-  user["questions"] = userStatsList[0];
-  user["answers"] = userStatsList[1];
-  user["essays"] = userStatsList[2];
-  user["bookmarks"] = userStatsList[3];
-  user["edits"] = userStatsList[4];
-  user["profile_views"] = $("div.zm-side-section-inner > span > strong").text(); 
+    $ = cheerio.load(body);
+    user["name"] = $("span.name").text();
+    user["location"] = $("span.location").attr('title');
+    var followDiv = $("div.zm-profile-side-following > a > strong");
+    var followList = serialize($, followDiv);
+    user["following"] = followList[0];
+    user["followers"] = followList[1];
+    var userStatsDiv = $("div.profile-navbar > a.item > span.num");
+    var userStatsList = serialize($, userStatsDiv);
+    user["questions"] = userStatsList[0];
+    user["answers"] = userStatsList[1];
+    user["essays"] = userStatsList[2];
+    user["bookmarks"] = userStatsList[3];
+    user["edits"] = userStatsList[4];
+    user["profile_views"] = $("div.zm-side-section-inner > span > strong").text(); 
  
-  console.log(user);
+    console.log(user);
+    http.createServer(function(request, response) {
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.write(JSON.stringify(user, undefined, 2));
+      response.end();
+    }).listen(8888);
 }
 
 function serialize($, object) {
