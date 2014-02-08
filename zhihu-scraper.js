1198a540ed4c4cb89101ac967c84dbc3b176efa6
Brian Zhang <tangerinewhite32@gmail.com>
2014-02-08 04:29:14 +0000

    init

---
+++ zhihu-scraper.js
@@ -0,0 +1,33 @@
+var request = require("request");
+var cheerio = require("cheerio");
+ 
+var user = {};
+request({ uri: "http://www.zhihu.com/people/jixin", }, parseZhihuUser);
+
+function parseZhihuUser(error, response, body) {
+  $ = cheerio.load(body);
+  user["name"] = $("span.name").text();
+  user["location"] = $("span.location").attr('title');
+  var followDiv = $("div.zm-profile-side-following > a > strong");
+  var followList = serialize($, followDiv);
+  user["following"] = followList[0];
+  user["followers"] = followList[1];
+  var userStatsDiv = $("div.profile-navbar > a.item > span.num");
+  var userStatsList = serialize($, userStatsDiv);
+  user["questions"] = userStatsList[0];
+  user["answers"] = userStatsList[1];
+  user["essays"] = userStatsList[2];
+  user["bookmarks"] = userStatsList[3];
+  user["edits"] = userStatsList[4];
+  user["profile_views"] = $("div.zm-side-section-inner > span > strong").text(); 
+ 
+  console.log(user);
+}
+
+function serialize($, object) {
+    var list = [];
+    object.each(function() {
+        list.push($(this).text());
+    });
+    return list;
+}
