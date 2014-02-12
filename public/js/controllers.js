3f53e24616aec13c9f95c59da0a66ba5ff27cf49
Brian Zhang <tangerinewhite32@gmail.com>
2014-02-12 02:55:02 +0000

    applying Zhihu username from webpage form

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,8 +2,9 @@
 
 /* Controllers */
 function UserSearch($scope, $http) {
+    console.log($scope);
     $scope.getZhihuUser = function() {
-        $http({method: 'GET', url: '/api/zhihuUser'}).success(function(data, status, headers, config) {
+        $http({method: 'GET', url: '/api/zhihuUser?username='+$scope.username}).success(function(data, status, headers, config) {
             $scope.user = data;
             console.log($scope.user);
         }).error(function(data, status, headers, config) {
